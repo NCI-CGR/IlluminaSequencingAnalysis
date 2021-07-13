fd8521a69ccfbe1616ceffce36d9266e131aeb77
Xin Li <xin.li4@nih.gov>
2021-07-13 17:25:51 -0400

    Fix Bug: unexpected warning email Details: Avoid the warning email (tput: No value for $TERM and no -T specified) when corntab job been processed.
    ref #15

--- CustomizedQC/Crontab/run.sh
+++ CustomizedQC/Crontab/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source /etc/profile.d/modules.sh
 module load python/3.7
 
 python3 /home/lix33/lxwg/Git/IlluminaSequencingAnalysis/CustomizedQC/SourceCode/CustomizedQC.py /data/COVID_WGS/primary_analysis/COVID19/06_30_2021/ProcessedData
