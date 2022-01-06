b73e66a1714b237685da5156509c2564f03547fd
Xin Li <xin.li4@nih.gov>
2022-01-06 16:16:16 -0500

    New Features: Retrieve BAM from S3 Details: 1: MatchSampleWithS3Archive   (1) Check if there is any duplicate samples   (2) Check the number of Sample that do not contain BAM Path 2: RetrieveDataFromS3   (1) Skip duplicate sample and empty sample   (2) Create SLRUM job for each sample 3: RetrieveFileFromS3.sh   (1) Bash script retrieve each single CGR sample   (2) Include both BAM and BAI   (3) Record the time consuming 4: Update Lisa's excel file   (1) All duplicated samples have been removed   (2) There are still 5 empty samples that cannot be found in USU dataset.

--- Ad-hoc/Cenk/SourceCode/GetFileFromS3.py
+++ Ad-hoc/Cenk/SourceCode/GetFileFromS3.py
@@ -7,14 +7,57 @@ import subprocess
 import sys
 import os
 
+DESTDir = "/data/COVID_ADHOC/Sequencing/COVID_WGS/Data/USU_First_Batch_low_std_96_361_18"
+LOGDir = DESTDir + "/Log" 
+
 class ClsSample:
     def __init__(self):
         self.strCGRID = ""
-        self.strBAM = ""        
+        self.strBAM = ""
+        self.bDup = False
     
     def Init(self, strCGRID):
-        self.strCGRID = strCGRID 
-
+        self.strCGRID = strCGRID
+    
+    def RetrieveDataFromS3(self):
+        # 1: Check if BAM is existed
+        if self.strBAM == "":
+            print("Error: BAM file is not existed in S3!", " ---> Sample ID:", self.strBAM)
+            return
+        
+        if not os.path.exists(DESTDir):
+            print("Error: Dest Dir is not existed!")
+            return
+        
+        if self.bDup:
+            print("Warning: duplicate sample, skip it!", " ---> Sample ID:", self.strBAM)
+            return
+        
+        strFileName = os.path.basename(self.strBAM)
+        strBashScript = ("bash " + "RetrieveFileFromS3.sh " + " '" + self.strBAM + "'" + 
+                                                              " '" + strFileName + "'" +
+                                                              " '" + DESTDir + "'")
+        
+        # 2: Submit jobs
+        strNumCore = "1"
+        strNodeNum = "1"
+        strJobName = "RTS3-" + self.strCGRID
+                     
+        strRunningTime = "3-00:00:00"
+        strStdOut = LOGDir + "/" + strFileName.split('.')[0] + ".std.out"
+        strStdErr = LOGDir + "/" + strFileName.split('.')[0] + ".std.err"        
+                        
+        CMD = ("sbatch " + "--ntasks=" + strNumCore + " " +  
+                           "--nodes=" + strNodeNum + " " +
+                           "--job-name=" + strJobName + " " + 
+                           "--time=" + strRunningTime + " " + 
+                           "--output=" + strStdOut + " " +
+                           "--error=" + strStdErr + " " + 
+                           "--wrap=\"" + strBashScript + "\"")
+        
+        print(CMD)
+        os.system(CMD)                                
+        print("Retrieving BAM ->", self.strCGRID, "\n")   
 
 def InitSamples(vSample):
     CMD = "awk -F ',' '{print $1}' ../Files/COVIDwgs_Jan2022_NIAIDphenotypesLM_Comma_Delimited.csv | tail -n +2"
@@ -31,6 +74,7 @@ def InitSamples(vSample):
     for sample in vSample:
         if sample.strCGRID in dictIDList:
             dictIDList[sample.strCGRID] += 1
+            sample.bDup = True/data/COVID_ADHOC/Sequencing/COVID_WGS/Data/USU_First_Batch_low_std_96_361_18/Log
         else:
             dictIDList[sample.strCGRID] = 1
     
@@ -63,12 +107,18 @@ def MatchSampleWithS3Archive(vSample):
             print(sample.strCGRID)
             iEmptyBAM += 1
     print(" ********", "\n", "iEmptyBAM:", iEmptyBAM, "\n", "********", "\n")
+    
+def RetrieveDataFromS3(vSample):
+    for sample in vSample:
+        sample.RetrieveDataFromS3()
+        #return
 
 def main():
     #strExcel= sys.argv[1]
     vSample = []
     InitSamples(vSample)
     MatchSampleWithS3Archive(vSample)
+    RetrieveDataFromS3(vSample)
 
 if __name__ == "__main__":
     main()
---
+++ Ad-hoc/Cenk/SourceCode/RetrieveFileFromS3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -o pipefail
+
+argIndex=1
+for arg in "$@"
+do
+    if [[ ${argIndex} == 1 ]]; then
+        strBAMFileS3=${arg}
+    elif [[ ${argIndex} == 2 ]]; then
+        strDestFileName=${arg}    
+    elif [[ ${argIndex} == 3 ]]; then
+        strDestDir=${arg}
+    fi
+    argIndex=$((argIndex + 1))
+done
+
+SECONDS=0
+
+# Get BAM
+CMD="obj_get -v DCEG_COVID_WGS ${strBAMFileS3} -D ${strDestDir} -p -V --strip 12"
+echo "${CMD}"
+eval "${CMD}"
+
+echo 
+
+# Get BAI
+CMD="obj_get -v DCEG_COVID_WGS ${strBAMFileS3}.bai -D ${strDestDir} -p -V --strip 12"
+echo "${CMD}"
+eval "${CMD}"
+
+echo 
+
+# Check Running Results
+result=$?
+duration=$SECONDS
+echo "Running Time: $(($duration / 3600))hrs $((($duration / 60) % 60))min $(($duration % 60))sec"
+
+if [[ ${result} -eq 0 ]]; then
+  echo "$(date): ${strDestFileName} has been reteieved successfully!"
+else
+  echo "Error: $(date) ${strDestFileName} was failed to be retrieved!"
+  exit 1
+fi
