22ec6a6a8c23af07d842212c59bc1af5c5c3638b
Xin Li <xin.li4@nih.gov>
2022-01-10 17:45:08 -0500

    New features: Retreving data from S3 dynamically Details: 1: Change the retrieving output folder from USU_First_Batch_low_std_96_361_18 to    USU_First_Batch_low_std_96_361_18/debug 2: New function: always maintain maximum 20 jobs in running pool automatically & dynamically 3: Correct the print error if CGR ID 4: Print date in log file to make it be better readable. 5: Evaluate if need to add "--checksum" in obj_get 6: Use flag file to control the number of concurrency jobs 7: Deploy crontab job in biowulf cluster
    ref #51

--- Ad-hoc/Cenk/SourceCode/GetFileFromS3.py
+++ Ad-hoc/Cenk/SourceCode/GetFileFromS3.py
@@ -7,8 +7,12 @@ import subprocess
 import sys
 import os
 
-DESTDir = "/data/COVID_ADHOC/Sequencing/COVID_WGS/Data/USU_First_Batch_low_std_96_361_18"
-LOGDir = DESTDir + "/Log" 
+# DESTDir = "/data/COVID_ADHOC/Sequencing/COVID_WGS/Data/USU_First_Batch_low_std_96_361_18"
+# LOGDir = DESTDir + "/Log"
+
+DESTDir = "/data/COVID_ADHOC/Sequencing/COVID_WGS/Data/USU_First_Batch_low_std_96_361_18/debug"
+LOGDir = DESTDir + "/Log"
+FLAGDir = DESTDir + "/Flag" 
 
 class ClsSample:
     def __init__(self):
@@ -22,21 +26,35 @@ class ClsSample:
     def RetrieveDataFromS3(self):
         # 1: Check if BAM is existed
         if self.strBAM == "":
-            print("Error: BAM file is not existed in S3!", " ---> Sample ID:", self.strBAM)
-            return
+            print("Error: BAM file is not existed in S3!", " ---> Sample ID:", self.strCGRID, "\n")
+            return 0
         
         if not os.path.exists(DESTDir):
             print("Error: Dest Dir is not existed!")
-            return
+            return 0
         
         if self.bDup:
-            print("Warning: duplicate sample, skip it!", " ---> Sample ID:", self.strBAM)
-            return
+            print("Error: duplicate sample, skip it!", " ---> Sample ID:", self.strCGRID, "\n")
+            return 0
         
         strFileName = os.path.basename(self.strBAM)
+        strWorkingFlag = FLAGDir + "/" + strFileName + ".flag.working"
+        strDoneFlag = FLAGDir + "/" + strFileName + ".flag.done"
+        
+        if os.path.exists(strDoneFlag):
+            return 0
+        
+        if os.path.exists(strWorkingFlag):
+            return 1
+        
         strBashScript = ("bash RetrieveFileFromS3.sh" + " '" + self.strBAM + "'" + 
                                                         " '" + strFileName + "'" +
-                                                        " '" + DESTDir + "'")
+                                                        " '" + DESTDir + "'" + 
+                                                        " '" + strWorkingFlag + "'" + 
+                                                        " '" + strDoneFlag + "'")
+        
+        CMD = "touch " + strWorkingFlag
+        os.system(CMD)
         
         # 2: Submit jobs
         strNumCore = "1"
@@ -57,7 +75,8 @@ class ClsSample:
         
         print(CMD)
         os.system(CMD)                                
-        print("Retrieving BAM ->", self.strCGRID, "\n")   
+        print("Retrieving BAM ->", self.strCGRID, "\n")
+        return 1
 
 def InitSamples(vSample):
     CMD = "awk -F ',' '{print $1}' ../Files/COVIDwgs_Jan2022_NIAIDphenotypesLM_Comma_Delimited.csv | tail -n +2"
@@ -109,11 +128,23 @@ def MatchSampleWithS3Archive(vSample):
     print(" ********", "\n", "iEmptyBAM:", iEmptyBAM, "\n", "********", "\n")
     
 def RetrieveDataFromS3(vSample):
+    iRunNum = 0 
     for sample in vSample:
-        sample.RetrieveDataFromS3()
-        #return
+        iRunNum += sample.RetrieveDataFromS3()
+        if iRunNum > 20: 
+            return
+    if iRunNum == 0:
+        print("Everything is all set!")
 
 def main():
+    # Print time stamp -->
+    print()
+    print("====== Get File From S3 ======", flush=True)
+    print("Excel:", "../Files/COVIDwgs_Jan2022_NIAIDphenotypesLM_Comma_Delimited.csv")
+    os.system("date")
+    print()
+    # <--
+    
     #strExcel= sys.argv[1]
     vSample = []
     InitSamples(vSample)
--- Ad-hoc/Cenk/SourceCode/RetrieveFileFromS3.sh
+++ Ad-hoc/Cenk/SourceCode/RetrieveFileFromS3.sh
@@ -11,13 +11,18 @@ do
         strDestFileName=${arg}    
     elif [[ ${argIndex} == 3 ]]; then
         strDestDir=${arg}
+    elif [[ ${argIndex} == 4 ]]; then
+        strWorkingFlag=${arg}
+    elif [[ ${argIndex} == 5 ]]; then
+        strDoneFlag=${arg}
     fi
     argIndex=$((argIndex + 1))
 done
 
 SECONDS=0
 
-# Get BAM
+# Get BAM (do not add --checksum this time if this option did not be used in obj_put)
+#CMD="obj_get -v DCEG_COVID_WGS --checksum ${strBAMFileS3} -D ${strDestDir} -p -V --strip 12"
 CMD="obj_get -v DCEG_COVID_WGS ${strBAMFileS3} -D ${strDestDir} -p -V --strip 12"
 echo "${CMD}"
 eval "${CMD}"
@@ -35,7 +40,8 @@ fi
 
 echo 
 
-# Get BAI
+# Get BAI (do not add --checksum this time if this option did not be used in obj_put)
+#CMD="obj_get -v DCEG_COVID_WGS --checksum ${strBAMFileS3}.bai -D ${strDestDir} -p -V --strip 12"
 CMD="obj_get -v DCEG_COVID_WGS ${strBAMFileS3}.bai -D ${strDestDir} -p -V --strip 12"
 echo "${CMD}"
 eval "${CMD}"
@@ -47,6 +53,11 @@ echo "Running Time: $(($duration / 3600))hrs $((($duration / 60) % 60))min $(($d
 
 if [[ ${result} -eq 0 ]]; then
   echo "$(date): BAI has been reteieved successfully!"
+  
+  # Update Flag
+  rm ${strWorkingFlag}
+  touch ${strDoneFlag}
+  
 else
   echo "Error: $(date) BAI was failed to be retrieved!"
   exit 1
